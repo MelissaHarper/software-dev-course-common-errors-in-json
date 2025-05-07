e8efbb5e2e854982418c83cc3e26ee07dfe0d55e
Harper <HarperMelissaM@gmail.com>
2025-05-07 14:02:48 -0500

    edited code syntax, answered questions

--- project-task.js
+++ project-task.js
@@ -44,30 +44,30 @@ but it contains multiple syntax errors that prevent it from being parsed.
 // 🔍 Provided JSON File with Errors
 // ============================================
 
-const invalidBookingJSON = `
+// Deleted variable assignment and backtick. Cannot declare variables in JSON and backticks here and at the end are superfluous and not allowed.
 {
   "hotelName": "Grand City Hotel",
-  "checkInDate": "2024-05-15"
+  "checkInDate": "2024-05-15", // Added a comma to differentiate between this and next key-value pair
   "checkOutDate": "2024-05-20",
   "guests": [
     {
-      name: "Alice Johnson",
+      "name": "Alice Johnson", //Added double quotes to key as they are required for keys in JSON syntax
       "age": 30,
       "email": "alice.johnson@example.com"
     },
     {
       "name": "Bob Smith",
-      "age": undefined,
+      "age": null, // Changed "undefined" to null as undefined is not recognized in JSON
       "email": "bob.smith@example"
     }
   ],
   "roomDetails": {
     "type": "Suite",
     "pricePerNight": 200,
-    "amenities": ["WiFi", "Breakfast", "Parking",]
+    "amenities": ["WiFi", "Breakfast", "Parking"] // Deleted trailing comma as JSON does not ignore them like JavaScript does.
   }
 }
-`;
+// Deleted unnecessary backtick and semicolon that are not allowed in JSON
 
 
 // ============================================
@@ -91,11 +91,15 @@ const invalidBookingJSON = `
 💬 Reflect and answer the following:
 
 1️⃣ What tools or techniques did you use to identify the errors?
+I use VS Code as an editor and have extensions that verify the code syntax
 
 2️⃣ How did you confirm that your corrected JSON file was valid?
+I created a JSON file and pasted the edited syntax there. For extra measure, I checked the code in codebeautify as well.
 
 3️⃣ Which errors were the most difficult to spot? Why?
+The trailing comma was the most difficult for me because it's so tiny and right between double quotations and a bracket making it easily overlooked.
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
+   My code editor seems pretty straightforward and thorough in catching errors. Throwing the code in the online validator adds a second verification confirmation.
 */
